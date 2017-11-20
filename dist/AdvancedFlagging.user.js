f2545c1e2d09887d9e558a60023cc8d1511b68ee
Robert Rudman <rjrudman@gmail.com>
2017-11-20 16:03:58 +1100

    Fix build

--- dist/AdvancedFlagging.user.js
+++ dist/AdvancedFlagging.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Advanced Flagging
 // @namespace    https://github.com/SOBotics
-// @version      0.2.10
+// @version      0.3.0
 // @author       Robert Rudman
 // @match        *://*.stackexchange.com/*
 // @match        *://*.stackoverflow.com/*

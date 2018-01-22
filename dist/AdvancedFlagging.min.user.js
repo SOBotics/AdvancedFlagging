0259194049c0ac51886314d11e2187f8bd7439ec
Robert Rudman <rjrudman@gmail.com>
2018-01-22 15:24:25 +1100

    Publish new version

--- dist/AdvancedFlagging.min.user.js
+++ dist/AdvancedFlagging.min.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Advanced Flagging
 // @namespace    https://github.com/SOBotics
-// @version      0.5.1
+// @version      0.5.2
 // @author       Robert Rudman
 // @match        *://*.stackexchange.com/*
 // @match        *://*.stackoverflow.com/*

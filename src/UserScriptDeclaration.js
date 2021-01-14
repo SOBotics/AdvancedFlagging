ff69de334bd55f37c149b7497c17890a29b9d794
double-beep <doublebeep7@gmail.com>
2021-01-14 21:05:15 +0000

    FIX #46: don't run on user's profile page

--- src/UserScriptDeclaration.js
+++ src/UserScriptDeclaration.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Advanced Flagging
 // @namespace    https://github.com/SOBotics
-// @version      1.3.3
+// @version      1.3.4
 // @author       Robert Rudman
 // @match        *://*.stackexchange.com/*
 // @match        *://*.stackoverflow.com/*

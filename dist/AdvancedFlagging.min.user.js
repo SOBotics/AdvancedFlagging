0ee1a30a2a6c43fe2c08d5c1d067fedc9478e298
double-beep <doublebeep7@gmail.com>
2021-01-14 09:12:35 +0000

    Bump version number

--- dist/AdvancedFlagging.min.user.js
+++ dist/AdvancedFlagging.min.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Advanced Flagging
 // @namespace    https://github.com/SOBotics
-// @version      1.3.2
+// @version      1.3.3
 // @author       Robert Rudman
 // @match        *://*.stackexchange.com/*
 // @match        *://*.stackoverflow.com/*

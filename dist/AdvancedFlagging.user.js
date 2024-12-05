d1d8ba8358c5d007db4da00e92679e50a8189556
double beep <38133098+double-beep@users.noreply.github.com>
2024-12-05 18:06:55 +0000

    2.1.0

--- dist/AdvancedFlagging.user.js
+++ dist/AdvancedFlagging.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Advanced Flagging
 // @namespace    https://github.com/SOBotics
-// @version      2.0.5
+// @version      2.1.0
 // @author       Robert Rudman
 // @contributor  double-beep
 // @match        *://*.stackexchange.com/*

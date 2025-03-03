ab011c3585efa4588051a40c0f08136589742774
double beep <38133098+double-beep@users.noreply.github.com>
2025-03-03 09:42:20 +0000

    fix: bring back Sentinel URLs

--- src/UserscriptTools/NattyApi.ts
+++ src/UserscriptTools/NattyApi.ts
@@ -99,7 +99,7 @@ export class NattyAPI extends Reporter {
     public override getIcon(): HTMLDivElement {
         return this.createBotIcon(
             this.wasReported()
-                ? `//logs.sobotics.org/Natty/${this.id}.html`
+                ? `//sentinel.sobotics.org/posts/aid/${this.id}`
                 : ''
         );
     }
